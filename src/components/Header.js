4ac2caf447513a68717c31e7c9a8e72000ec1afe
komangajun <wenten66@gmail.com>
2022-06-20 12:05:09 +0800

    first commit todoapp

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Header = () => {
+    return(
+                <div className="header">
+                <h1>CREATE YOUR TO DO LIST</h1>
+                </div>        
+    )
+}
+
+export default Header;
